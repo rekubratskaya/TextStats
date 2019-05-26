2992d4a559d0613679ac75374dfddc6c385d24c9
rekubratskaya <lenarekubratska@gmail.com>
2019-05-26 09:20:50 +0300

    DB concept -> Files proceeding concept

--- webapp/texting.py
+++ webapp/texting.py
@@ -1,6 +1,6 @@
 import string
 import collections
-from os import listdir
+import os
 from langdetect import detect
 
 """
@@ -9,7 +9,7 @@ scipy: Chi-square test of independence of variables in a contingency table
 from scipy.stats import chi2_contingency
 
 from webapp.logger import function_logger
-from webapp.models import LetitbeDB
+# from webapp.models import LetitbeDB
 
 
 text_to_test = {
@@ -19,10 +19,6 @@ text_to_test = {
     'de': "Der Roman Hundert Jahre Einsamkeit begleitet sechs Generationen der Familie Buendía und hundert Jahre wirklichen Lebens in der zwar fiktiven Welt von Macondo."
 }
 
-print("Fuck You")
-for entry in LetitbeDB.select():
-    print(entry)
-
 
 class TextProceedStrategy:
 
@@ -31,7 +27,7 @@ class TextProceedStrategy:
         self.num = num
         self.res = dict()
 
-    # @function_logger
+    @function_logger
     def __text_to_words(self) -> list:
         """
         Strip punctuation from a text and split it into a list
@@ -40,7 +36,7 @@ class TextProceedStrategy:
         words = clear_words.split(' ')
         return words
 
-    # @function_logger
+    @function_logger
     def _letters_scrabble(self):
         """Split each word into equal parts"""
         chunks = list()
@@ -54,7 +50,7 @@ class TextProceedStrategy:
         self._calc_chunks_frequency(self.res)
 
     @staticmethod
-    # @function_logger
+    @function_logger
     def _calc_chunks_frequency(res):
         """
         Calculate each chunk frequency and update chunks dictionary with new values
@@ -74,9 +70,24 @@ class CalculateXi2Strategy(TextProceedStrategy):
 
     @function_logger
     def get_expected_array(self, iso2):
-        base_text = LetitbeDB.get(LetitbeDB.language == iso2)  #TODO вот здесь вот проблема !!!
-        base_letters_dict = TextProceedStrategy(base_text.text, 3)
-        base_letters_dict._letters_scrabble()
-        for k in self.res.keys():
-            expected_frequency = base_letters_dict.res.get(k, 0)
-            self.expected_values.append(expected_frequency)
+        # base_text = LetitbeDB.get(LetitbeDB.language == iso2)  #TODO вот здесь вот проблема !!!
+        # base_letters_dict = TextProceedStrategy(base_text.text, 3)
+        # base_letters_dict._letters_scrabble()
+
+        APP_ROOT = os.path.dirname(os.path.abspath(__file__))  # refers to application_top
+        APP_TMP = os.path.join(APP_ROOT, 'tmp')
+        entries = os.listdir(APP_TMP)
+
+        for entry in entries:
+            print(entry)
+            with open(os.path.join(APP_TMP, entry), 'r') as f:
+                data = f.read()
+                iso_data = detect(data)
+                if not iso_data == iso2:
+                    continue
+                base_letters_dict = TextProceedStrategy(data, 3)
+                base_letters_dict._letters_scrabble()
+                for k in self.res.keys():
+                    expected_frequency = base_letters_dict.res.get(k, 0)
+                    self.expected_values.append(expected_frequency)
+
